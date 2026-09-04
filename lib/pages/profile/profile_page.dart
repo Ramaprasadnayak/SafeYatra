b473c71c4607e117d2c8a5823eacff80ca93cf9f
Ramaprasadnayak <ramprasadnayak999@gmail.com>
2026-09-04 13:47:38 +0530

    modified profile

--- lib/pages/profile/profile_page.dart
+++ lib/pages/profile/profile_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:maruthimedical/services/get_detail.dart';
 import 'package:safeyatra/widgets/profile_card.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -10,24 +9,6 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  String username = "";
-  @override
-  void initState() {
-    super.initState();
-    getUsername();
-  }
-
-  Future<void> getUsername() async {
-    // final name = await loadTokenSub();
-    String name="ram";
-
-    if (name != null) {
-      setState(() {
-        username = name.length > 15 ? "${name.substring(0, 15)}..." : name;
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     final VoidCallback? changeTheme=null;
@@ -38,44 +19,30 @@ class _ProfilePageState extends State<ProfilePage> {
     final VoidCallback? logout=null;
 
     Map<int, List<Object?>> profileOptions = {
-      1: [Icons.light_mode_rounded, "Change Theme", true, changeTheme],
-      2: [Icons.phone_android_rounded, "Change Phone Number", false, changePhoneno],
-      3: [Icons.lock_reset_rounded, "Change Password", false, changePassword],
-      4: [Icons.support_agent_rounded, "Contact Us", false, contact],
-      5: [Icons.delete_forever_rounded, "Delete Account", false, deleteAcc],
-      6: [Icons.logout_rounded, "Logout", false, logout],
+      1: [Icons.light_mode_rounded, "Change Theme", changeTheme],
+      2: [Icons.phone_android_rounded, "Change Phone Number", changePhoneno],
+      3: [Icons.lock_reset_rounded, "Change Password", changePassword],
+      4: [Icons.support_agent_rounded, "Contact Us", contact],
+      5: [Icons.delete_forever_rounded, "Delete Account", deleteAcc],
+      6: [Icons.logout_rounded, "Logout", logout],
     };
     return SafeArea(
       child: SingleChildScrollView(
         child: Column(
           children: [
             SizedBox(height: 20),
-            // Column(children: [Text("hello, $username")]),
-            // SizedBox(height: 20),
             CircleAvatar(
               radius: 55,
               backgroundColor: Colors.grey.shade300,
               child: Icon(Icons.person, size: 60, color: Colors.grey.shade700),
             ),
             SizedBox(height: 15),
-            // OutlinedButton(
-            //   onPressed: () {
-            //     showModalBottomSheet(
-            //       context: context, 
-            //       builder: (context){
-            //         return ProfilePic();
-            //       },
-            //     );
-            //   }, 
-            //   child: const Text("Edit Profile")
-            // ),
             SizedBox(height: 20),
             Column(
               children: profileOptions.entries.map((entry) {
                 return MyCard(
                   prefixIcon: entry.value[0] as IconData,
                   text: entry.value[1] as String,
-                  addButton: entry.value[2] as bool,
                   onPress: entry.value[3] as VoidCallback?,
                 );
               }).toList(),
--- lib/widgets/profile_card.dart
+++ lib/widgets/profile_card.dart
@@ -1,47 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:safeyatra/providers/theme_provider.dart';
-import 'package:provider/provider.dart';
+// import 'package:safeyatra/providers/theme_provider.dart';
+// import 'package:provider/provider.dart';
 
 class MyCard extends StatelessWidget {
   final IconData prefixIcon;
   final String text;
-  final bool addButton;
-  final VoidCallback? onPress;
+  final VoidCallback onPress;
 
   const MyCard({
     super.key,
     required this.prefixIcon,
     required this.text,
-    required this.addButton,
-    this.onPress,
+    required this.onPress,
   });
-
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 22),
-          child: Row(
-            children: [
-              Icon(prefixIcon, size: 30,),
-              const SizedBox(width: 16),
-
-              Expanded(child: Text(text)),
-
-              if (addButton)
-                Expanded(
-                  child: SwitchListTile(
-                    value:context.watch<ThemeProvider>().themeMode==ThemeMode.dark,
-                    onChanged: (value) {
-                      context.read<ThemeProvider>().setTheme(
-                        value ? ThemeMode.dark : ThemeMode.light,
-                      );
-                    },
-                  ),
-                ),
-            ],
+      child: GestureDetector(
+        onTap: onPress,
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 22),
+            child: Row(
+              children: [
+                Icon(prefixIcon, size: 30,),
+                const SizedBox(width: 16),
+                Expanded(child: Text(text)),
+              ],
+            ),
           ),
         ),
       ),
